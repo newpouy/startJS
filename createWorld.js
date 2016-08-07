8be18ef6436ec216769c881feff705f8a198a09d
newpouy <newpouy@gmail.com>
2016-08-07 11:44:18 +0900

    babel setting for es6 Person class modulization

--- createWorld.js
+++ createWorld.js
@@ -22,15 +22,7 @@ while(notEarth){
 console.log();
 console.log();
 
-class Person {
-  constructor(name){
-    this.durablilty = 100;
-    this.name = name;
-  }
-  sayName(){
-    console.log('안녕 난 '+this.name+'라고 해 :)'+this.durablilty+'살까지 살수 있어');
-  }
-}
+import Person from './person';
 
 let adam = new Person('Adam Sandler');
 adam.sayName();
